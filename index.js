7e788c05907309f2d45543a2f0132d51831d3ff6
Thiago Borges <thiago.borges@armazemparaiba.com.br>
2022-10-07 16:52:46 -0300

    integrando funcao de cadastro com o back

--- index.js
+++ index.js
@@ -13,4 +13,29 @@ async function carregarAnimais() {
 
 }
 
+function cadastrarAnimal() {
+
+    const form = document.getElementById("form")
+    const input = document.getElementById("nome")
+    const url = "http://127.0.0.1:9060/animais"
+
+    form.onsubmit = async (event) => {
+        if (input.value.length <= 0) {
+            event.preventDefault()
+            alert(`informe um nome válido`)
+        } else {
+
+            await axios.post(url, {
+                nome: input.value,
+                cor: "red",
+                sexo: "masculino",
+                idade: 22,
+            })
+            alert(`Animal com nome ${input.value} cadastrado `)
+        }
+    }
+
+}
+
 carregarAnimais()
+cadastrarAnimal()
